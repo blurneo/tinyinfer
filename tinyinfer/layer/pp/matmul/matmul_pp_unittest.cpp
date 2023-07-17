6883db70aab1be41ecd7a04655561696c9e9ce57
BlurNeo <sichang.su@hotmail.com>
2023-07-17 23:08:26 +0800

    test time

--- tinyinfer/layer/pp/matmul/matmul_pp_unittest.cpp
+++ tinyinfer/layer/pp/matmul/matmul_pp_unittest.cpp
@@ -16,15 +16,23 @@ void random_vector(std::vector<float> &vec) {
 using namespace ti;
 
 int main() {
-    int M = 5, K = 9, N = 4;
+    int M = 64, K = 64, N = 64;
     std::vector<float> A(M * K);
     std::vector<float> B(K * N);
     std::vector<float> C1(M * N);
     std::vector<float> C2(M * N);
     random_vector(A);
     random_vector(B);
-    matmul_ref(M, K, N, A, B, C1);
-    matmul_pp_block_pack(M, K, N, A, B, C2);
+    __TIC__(REF)
+    for (int i = 0; i < 100; i++) {
+        matmul_ref(M, K, N, A, B, C1);
+    }
+    __TOC__(REF)
+    __TIC__(PP)
+    for (int i = 0; i < 100; i++) {
+        matmul_pp_block_pack(M, K, N, A, B, C2);
+    }
+    __TOC__(PP)
     CHECK_VEC_EQUAL_RET(C1, C2, -1, "C1 C2 not equal");
 
     return 0;
