1767227124d455c0e9f1fd9dd4ad377069b47c43
victorscsu <victorscsu@tencent.com>
2023-07-18 18:15:11 +0800

    linux build

---
+++ scripts/build_linux.sh
@@ -0,0 +1,65 @@
+# Commands to build on macOS
+
+SCRIPT_ROOT="$( cd "$(dirname "$0")" ; pwd -P)"
+
+if [ -z ${IOS_PLATFORM+x} ]; then
+    MAC_PLATFORM="LINUX"
+fi
+
+if [ -z ${IOS_ARCH+x} ]; then
+    MAC_ARCH=""
+fi
+
+if [ -z ${CMAKE_GENERATOR+x} ]; then
+    CMAKE_GENERATOR="Unix Makefiles"
+fi
+
+if [ -z ${BUILD_DIR+x} ]; then
+    BUILD_DIR=build-linux
+fi
+
+if [ -z ${BUILD_AND_INSTALL+x} ]; then
+    BUILD_AND_INSTALL=ON
+fi
+
+if [ -z ${TI_BUILD_TYPE+x} ]; then
+    TI_BUILD_TYPE=Release
+fi
+
+if [ -z ${TI_ENABLE_SANITIZER+x} ]; then
+    TI_ENABLE_SANITIZER=ON
+fi
+
+if [ -z ${TI_Install_PREFIX+x} ]; then
+    TI_Install_PREFIX="linux"
+fi
+
+if [ -z ${INSTALL_DIR+x} ]; then
+    INSTALL_DIR="release"
+fi
+
+
+###########################################
+mkdir -p $SCRIPT_ROOT/$BUILD_DIR
+cd $SCRIPT_ROOT/$BUILD_DIR
+
+# Generate projects
+echo "Generating TI projects"
+cmake $SCRIPT_ROOT/.. \
+    -G"$CMAKE_GENERATOR" \
+    -DCMAKE_BUILD_TYPE=$TI_BUILD_TYPE \
+    -DCMAKE_CONFIGURATION_TYPES=$TI_BUILD_TYPE \
+    -DTI_Install_PREFIX="$TI_Install_PREFIX" \
+    -DPLATFORM=$MAC_PLATFORM \
+    -DARCHS="$MAC_ARCH" \
+    -DINSTALL_DIR=$INSTALL_DIR \
+    -DTI_ENABLE_SANITIZER=$TI_ENABLE_SANITIZER \
+    || exit 1
+
+echo "TI projects generate successfully"
+
+if [ ${BUILD_AND_INSTALL} = "ON" ]; then
+    echo "Building TI"
+    cmake --build . --config $TI_BUILD_TYPE -- $@ || exit 1
+    echo "TI build successfully"
+fi
